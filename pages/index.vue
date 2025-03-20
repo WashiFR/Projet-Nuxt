15ee3ad3f9c4c56f54556fc12fd1505f986591b7
Loris <lorisweier@gmx.fr>
2025-03-20 15:55:55 +0100

    :sparkles: Create home page + test Vuetify

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-h1">Home Page</h1>
+
+        <v-btn prepend-icon="$vuetify">Test</v-btn>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
